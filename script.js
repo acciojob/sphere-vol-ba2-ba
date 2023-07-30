050e8366fcee81330931cbc91ca90ef260801969
ba2-ba <112476436+ba2-ba@users.noreply.github.com>
2023-07-30 23:41:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function volume_sphere() {
     //Write your code here
 
-	 function volume_sphere() {
+	
             var radius = document.getElementById('radius').value;
             radius = Math.abs(radius);
             var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
@@ -9,8 +9,5 @@ function volume_sphere() {
             document.getElementById('volume').value = volume;
             return false;
         }
- 
-
-} 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
